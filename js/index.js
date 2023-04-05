4a43238b8d8f6f870d70f26a9ec25d03e6d16e7c
Laringitis <cabrera8lara@gmail.com>
2023-04-05 15:36:57 -0300

    funciona eliminar producto

--- js/index.js
+++ js/index.js
@@ -23,6 +23,16 @@ producto.forEach((product) => {
         content.append(comprar);
 
         comprar.addEventListener("click", () =>{
+            toast.info('Producto agregado', {
+                position: "bottom-left",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                });
             carrito.push({
                 id:product.id,
                 nombre:product.nombre,
@@ -32,55 +42,71 @@ producto.forEach((product) => {
             console.log(carrito);
         });
 });
+
 const guardarCompra= () => {
-    localStorage.setItem ("guardarCompra", JSON.stringify(carrito))};
+    localStorage.setItem ("guardarCompra", JSON.stringify(carrito))
+};
 
+const construirCarrito=()=>{
+carritoConteiner.innerHTML="";
+carritoConteiner.style.display= "flex";
+const visualCarrito = document.createElement("div");
+    visualCarrito.className ="carrito__header";
+    visualCarrito.innerHTML = `<h2 class="carrito__header--titulo">Mis compras:</h2>`
+        carritoConteiner.append(visualCarrito);
 
-verCarrito.addEventListener("click", ()=>{
-    carritoConteiner.innerHTML="";
-    carritoConteiner.style.display= "flex";
-    const visualCarrito = document.createElement("div");
-        visualCarrito.className ="carrito__header";
-        visualCarrito.innerHTML = `<h2 class="carrito__header--titulo">Mis compras:</h2>`
-            carritoConteiner.append(visualCarrito);
-
-            const cerrarCarrito=document.createElement("h1");
-            cerrarCarrito.className="pointer";
-            cerrarCarrito.innerText="❌";
-            cerrarCarrito.addEventListener("click", ()=>{
-                carritoConteiner.style.display= "none";
-            })
-            visualCarrito.append(cerrarCarrito);
-
-    carrito.forEach((product)=>{
-        let carritoContent = document.createElement ("div");
-        carritoContent.className="carrito__lista";
-        carritoContent.innerHTML=`
-        <img class="img__producto" src= "${product.img}">
-        <h3 class="lista titulo__producto">${product.nombre}</h3>
-        <p class="lista">${product.precio}</p>
-        <button class="boton__delete">x</button>
-        `;
-            carritoConteiner.append(carritoContent)            
+        const cerrarCarrito=document.createElement("h1");
+        cerrarCarrito.className="pointer";
+        cerrarCarrito.innerText="❌";
+        cerrarCarrito.addEventListener("click", ()=>{
+            carritoConteiner.style.display= "none";
         })
+        visualCarrito.append(cerrarCarrito);
 
-    const total= carrito.reduce((acc,el) => acc + el.precio,0)
-        const totalBuying= document.createElement("div");
-        totalBuying.className= "total__contenido";
-        totalBuying.innerHTML= `total a pagar: ${total} $`;
-            carritoConteiner.append(totalBuying)
+carrito.forEach((product)=>{
+    const carritoIndividual = document.createElement ("div");
+    carritoIndividual.className="carrito__lista";
+    carritoIndividual.innerHTML=`
+    <img class="img__producto" src= "${product.img}">
+    <h3 class="lista titulo__producto">${product.nombre}</h3>
+    <p class="lista">${product.precio}</p>
+    <button class ="botonEliminarProducto">X</button>
+    `;
+    carritoConteiner.append(carritoIndividual)     
+
+    const eliminarProducto = (id) =>{
+        const foundId= carrito.find ((element) => element.id === id);
+        console.log (foundId)
     
-    let botonDelete = carritoContent.querySelector(".boton__delete")
-    botonDelete.addEventListener("click", ()=>{
-        borrarProducto(producto.id)
-    })
-    let borrarProducto = (id) =>{
-        let foundId= carrito.find((product) => product.id === id);
-        console.log(foundId);
-    
-        carrito = carrito.filter ((carritoId) => {
-            return carritoId !== foundId;
-        });
+        carrito=  carrito.filter((carrtitoId) => {
+            return carrtitoId !== foundId;
+        })
+        construirCarrito();
+        guardarCompra();
     }
-})
+
+    let eliminar =carritoIndividual.querySelector(".botonEliminarProducto")
+
+    eliminar.addEventListener("click", ()=>{
+        eliminarProducto(product.id);
+    })
+    // let eliminar= document.createElement("button")
+    //     eliminar.innerText="x"
+    //     carritoIndividual.append(eliminar)
+    //     eliminar.addEventListener("click", eliminarProducto)
+
+    })
+    
+    
+    const total= carrito.reduce((acc,el) => acc + el.precio,0)
+    const totalBuying= document.createElement("div");
+    totalBuying.className= "total__contenido";
+    totalBuying.innerHTML= `total a pagar: ${total} $`;
+    carritoConteiner.append(totalBuying)
+    
+    
+    
+}
+
+verCarrito.addEventListener("click", construirCarrito());
 
